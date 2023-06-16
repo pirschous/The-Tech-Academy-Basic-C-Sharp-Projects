c82ae491858f4cdb325da7b295e68d253ad569a9
pirschous <110510027+pirschous@users.noreply.github.com>
2023-06-15 21:19:42 -0700

    Add files via upload

---
+++ ShippingQuoteApp/ShippingQuoteApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingQuoteApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.WriteLine("Please enter the package weight:");
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+           
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Package is too heavy to be shipped via Package Express. Have a good day!");
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("Please enter the package width:");
+            int packageWidth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height:");
+            int packageHeight = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Please enter the package length:");
+            int packageLength = Convert.ToInt32(Console.ReadLine());
+
+            int dimensionTotal = packageWidth + packageHeight + packageLength;
+            if (dimensionTotal > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express. ");
+                Console.ReadLine();
+            }
+
+            int quote = (packageWidth * packageHeight * packageLength * packageWeight) / 100;
+            Console.WriteLine("Your estimated total for shipping this package is: $" + quote);
+            Console.WriteLine("Thank you!");
+
+            Console.ReadLine();
+        }
+    }
+}
