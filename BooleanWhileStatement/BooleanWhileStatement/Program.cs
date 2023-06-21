9dbdf9972d9e2380a0bd97a856a152de192cabff
pirschous <110510027+pirschous@users.noreply.github.com>
2023-06-20 19:50:54 -0700

    Add files via upload

---
+++ BooleanWhileStatement/BooleanWhileStatement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanWhileStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Comparison using while statement
+            int number = 5;
+            int limit = 10;
+
+            Console.WriteLine("Using a while statement:");
+            while (number < limit)
+            {
+                Console.WriteLine("Number: " + number);
+                number++;
+            }
+            Console.WriteLine();
+
+            // Comparison using a do-while statement
+            int count = 0;
+            int target = 3;
+
+            Console.WriteLine("Using a do-while statement:");
+            do
+            {
+                Console.WriteLine("Count: " + count);
+                count++;
+            } while (count < target);
+
+            Console.ReadLine();
+        }
+    }
+}
