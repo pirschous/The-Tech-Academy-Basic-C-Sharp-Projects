7f4fca3f6774f8f66b4e42816e7a9cd3d0d4e679
pirschous <110510027+pirschous@users.noreply.github.com>
2023-06-29 16:48:24 -0700

    Add files via upload

---
+++ MathOperations/MathOperations/Operators.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    public class Operators
+    {
+
+        public int Square(int number)
+        {
+            return number * number;
+        }
+
+        public int Double(int number)
+        {
+            return number * 2;
+        }
+
+        public int Add(int number)
+        {
+            return number + 25;
+        }
+    }
+}
---
+++ MathOperations/MathOperations/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number:");
+            int userInput = int.Parse(Console.ReadLine());
+
+            Operators operators = new Operators();
+
+            int squaredNumber = operators.Square(userInput);
+            Console.WriteLine($"Squared number: {squaredNumber}");
+
+            int doubledNumber = operators.Double(userInput);
+            Console.WriteLine($"Doubled number: {doubledNumber}");
+
+            int numberPlus = operators.Add(userInput);
+            Console.WriteLine($"Number plus twenty five: {numberPlus}");
+
+            Console.ReadLine();
+        }
+    }
+}
