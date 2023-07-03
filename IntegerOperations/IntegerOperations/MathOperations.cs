621663db256b4f8dbccd977cc5e132432aaa7843
pirschous <110510027+pirschous@users.noreply.github.com>
2023-07-03 16:24:27 -0700

    Add files via upload

---
+++ IntegerOperations/IntegerOperations/MathOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerOperations
+{
+    class MathOperations
+    {
+
+        // Defins a void method that takes in two integers
+        public void PerformMathOperation(int firstNumber, int secondNumber)
+        {
+            // Perform a math operation on the first number and display the second number
+            int result = firstNumber + 5;
+            Console.WriteLine("The second number is: " + secondNumber);
+        }
+    }
+}
---
+++ IntegerOperations/IntegerOperations/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiates the class
+            MathOperations math = new MathOperations();
+
+            // Calls the PerformMathOperation method
+            math.PerformMathOperation(17, 23);
+
+            // Calls the PerformMathOperation method, specifying by name
+            math.PerformMathOperation(firstNumber: 7, secondNumber: 17);
+
+            // Wait for user input
+            Console.ReadLine();
+        }
+    }
+}
