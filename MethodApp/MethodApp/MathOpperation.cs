4412e090593611442f95a322d8b8d3953768b8c6
pirschous <110510027+pirschous@users.noreply.github.com>
2023-07-02 21:49:15 -0700

    Add files via upload

---
+++ MethodApp/MethodApp/MathOpperation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodApp
+{
+    class MathOpperation
+    {
+        //Crate method that takes 2 integers as [arameters
+        //Make the second one number optional with a default value of 0
+        public int PerformOperation(int number1, int number2 = 0)
+        {
+            // Perform math operation
+            int result = number1 + number2;
+
+            return result;
+
+        }
+    }
+}
---
+++ MethodApp/MethodApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOpperation mathOpperation = new MathOpperation();
+
+            Console.WriteLine("Enter the first number:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number (press Enter to skip):");
+            string input = Console.ReadLine();
+
+            int num2;
+
+            if (string.IsNullOrEmpty(input))
+            {
+                // If the user skipped, set to default value
+                num2 = 0;
+            }
+            else
+            {
+                // Parse the user's input as an int
+                num2 = int.Parse(input);
+            }
+
+            // Call the PerformOperation method, passing in the one or two numbers
+            int result = mathOpperation.PerformOperation(num1, num2);
+
+            // Displat the result
+            Console.WriteLine("The result is: " + result);
+
+            Console.ReadLine();
+        }
+    }
+}
