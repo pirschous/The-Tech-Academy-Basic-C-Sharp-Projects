28dab3d542f05cb3654c96e446c7c3fa889e28a0
pirschous <110510027+pirschous@users.noreply.github.com>
2023-07-04 16:54:49 -0700

    Add files via upload

---
+++ DivideByTwo/DivideByTwo/MyClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideByTwo
+{
+    public class MyClass
+    {
+        // Void method that divided the input by 2
+        public void DivideByTwo(int input)
+        { 
+            
+            int result = input / 2;
+            Console.WriteLine("Result: " + result);
+        }
+
+        //Method with output parameters
+        public void Multiply(int a, int b, out int product)
+        {
+            product = a * b;
+        }
+
+        // Method overload
+        public void Multiply(int a, int b)
+        {
+            int product = a * b;
+            Console.WriteLine("Product: " + product);
+        }
+    }
+
+    // Static class decleration
+    public static class StaticClass
+    {
+        public static void StaticMethod()
+        {
+            Console.WriteLine("This is a static method.");
+        }
+    }
+}
---
+++ DivideByTwo/DivideByTwo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideByTwo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Insantiates ...
+            MyClass myObject = new MyClass();
+
+            // User enters number
+            Console.WriteLine("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // Call DivideByTwo method and display it
+            myObject.DivideByTwo(number);
+
+            // Call Multiply method with output parameters
+            int product;
+            myObject.Multiply(5, 10, out product);
+            Console.WriteLine("Output parameter product: " + product);
+
+            // Call Multiply method overload
+            myObject.Multiply(3, 7);
+
+            // Call StaticMethod from StaticClass
+            StaticClass.StaticMethod();
+
+            // Wait for user input before closing the console window
+            Console.ReadLine();
+
+        }
+    }
+}
