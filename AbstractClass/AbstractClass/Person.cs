1f518e09540b6bf2a57e2ca4fd335295198f8780
pirschous <110510027+pirschous@users.noreply.github.com>
2023-07-05 19:03:46 -0700

    Add files via upload

---
+++ AbstractClass/AbstractClass/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Give it the method SayName()
+        public abstract void SayName();
+    }
+}
---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Instantiates an Employee object in Main() method with firstName and lastName
+            Employee employee = new Employee
+            {
+                FirstName = "James",
+                LastName = "Pirsch"
+            };
+
+            // Calls the SayName() method on the object
+            employee.SayName();
+        }
+    }
+}
