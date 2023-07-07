cd28144f32bcac10b1246f1d4d2e4d088748aa35
pirschous <110510027+pirschous@users.noreply.github.com>
2023-07-07 12:52:00 -0700

    Add files via upload

---
+++ PolymorphismSub/PolymorphismSub/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismSub
+{
+    public class Employee : IQuittable
+    {
+
+        public string Name { get; set; }
+
+        // Implement the Quit() method from the IQuittable interface
+        public void Quit()
+        {
+            Console.WriteLine("Employee {0} is quitting.", Name);
+            // Additional logic can be added here if needed
+        }
+    }
+}
---
+++ PolymorphismSub/PolymorphismSub/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismSub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Use to create an object of type IQuittable and call the Quit() method
+
+            // Create an instance of the Employee class
+            Employee employee = new Employee { Name = "James" };
+
+            // Creat an object of type IQuittable and assign employee instance
+            IQuittable quittable = employee;
+
+            // Call the Quit() method on the quittable object
+            quittable.Quit();
+
+            // Wait for user input before closing the console window
+            Console.ReadLine();
+        }
+    }
+}
