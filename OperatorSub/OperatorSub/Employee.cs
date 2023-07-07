4e5fa1018e869085755264e94b784910e4fe3b66
pirschous <110510027+pirschous@users.noreply.github.com>
2023-07-07 16:05:01 -0700

    Add files via upload

---
+++ OperatorSub/OperatorSub/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorSub
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }  
+        public string LastName { get; set; }   
+
+        // Overload the == operator to compare Employee objects
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            if (ReferenceEquals(emp1, emp2))
+                return true;
+
+            if (ReferenceEquals(emp1, null) || ReferenceEquals(emp2, null))
+                return false;
+
+            return emp1.Id == emp2.Id;
+        }
+
+        // Overloading the != operator by using the previously overloaded ==
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return !(emp1 == emp2);
+        }
+    }
+}
---
+++ OperatorSub/OperatorSub/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorSub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate 2 opbjects of Employee class and assign value
+            Employee employee1 = new Employee
+            {
+                Id = 1,
+                FirstName = "James",
+                LastName = "Pirch"
+            };
+
+            Employee employee2 = new Employee
+            {
+                Id = 2,
+                FirstName = "Stephanie",
+                LastName = "Pirsch"
+            };
+
+            // Compare the 2 Employee objects using the overload operator
+            bool areEqual = employee1 == employee2;
+
+            // Display results
+            Console.WriteLine("Are the employees equal? " + areEqual);
+
+            // Wait for user input to close console
+            Console.ReadLine();
+        }
+    }
+}
