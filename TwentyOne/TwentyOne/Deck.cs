52fa977ee9cab887813a4aa0b765578b120ace6b
pirschous <110510027+pirschous@users.noreply.github.com>
2023-07-10 17:08:37 -0700

    Add files via upload

---
+++ TwentyOne/TwentyOne/Deck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Deck
+    {
+        public List<Card> Cards { get; set; }
+
+        public void Shuffle( int times = 1)
+        {
+            
+            for (int i = 0; i < times; i++)
+            {
+                
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count);
+                    TempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                Cards = TempList;
+            }
+        }
+    }
+}
