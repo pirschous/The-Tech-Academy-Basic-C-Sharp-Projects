f7179f738fc5a2d228821979ef061ed54f8f706c
pirschous <110510027+pirschous@users.noreply.github.com>
2023-07-12 17:19:54 -0700

    Add files via upload

---
+++ TwentyOne/Casino/Deck.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    public class Deck
+    {
+        public List<Card> Cards { get; set; }
+
+        public Deck()
+        {
+            Cards = new List<Card>();
+
+            for (int i = 0;  i < 4; i++)
+            {
+                for (int j = 0; j < 13; j ++)
+                {
+                    Card card = new Card();
+                    card.Suit = (Suit)i;
+                    card.Face = (Face)j;
+                    Cards.Add(card);
+                }
+            }
+        }
+
+        public void Shuffle( int times = 1)
+        {
+            
+            for (int i = 0; i < times; i++)
+            {
+                
+                List<Card> TempList = new List<Card>();
+                Random R = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = R.Next(0, Cards.Count);
+                    TempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                Cards = TempList;
+            }
+        }
+
+        public void Shuffle(out int timesShuffled, out string test, int times = 1)
+        {
+            timesShuffled = 0;
+            test = "Second out parameter";
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> Templist = new List<Card>();
+                Random R = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = R.Next(0, Cards.Count);
+                    Templist.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                Cards = Templist;
+                timesShuffled++;
+            }
+        }
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Casino;
+using Casino.TwentyOne;
 
 namespace TwentyOne
 {
@@ -10,6 +12,7 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
             string playerName = Console.ReadLine();
             Console.WriteLine("And how much money did you bring today?");
