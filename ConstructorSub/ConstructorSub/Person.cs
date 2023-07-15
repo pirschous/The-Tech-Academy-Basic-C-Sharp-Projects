f897524620fd1a12df1d56632b5be45724d068e0
pirschous <110510027+pirschous@users.noreply.github.com>
2023-07-14 17:29:26 -0700

    Add files via upload

---
+++ ConstructorSub/ConstructorSub/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSub
+{
+    class Person
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public Person(string fullName) : this(fullName, "")
+        {
+
+        }
+
+        public string GetFullName()
+        {
+            return $"{FirstName} {LastName}".Trim();
+        }
+    }
+}
---
+++ ConstructorSub/ConstructorSub/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creats a const variable
+            const int aConstVariable = 10;
+
+            // Creates a variable using "var"
+            var person = new Person("James", "Pirsch");
+
+            Console.WriteLine(aConstVariable);
+            Console.WriteLine(person.GetFullName());
+            Console.ReadLine();
+        }
+    }
+}
